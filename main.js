c758aae3365a916de61aa1082a9dd4151dc1b100
ycleo <leoyclee@gmail.com>
2023-07-17 19:01:15 +0800

    test ssh

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const scene = new THREE.Scene()
 const geometry = new THREE.SphereGeometry(3, 64, 64)
 const material = new THREE.MeshStandardMaterial({
   color: '#00ff83',
-  roughness: 0.4,
+  roughness: 0.3,
 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
